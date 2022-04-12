bf8a1d31cf24e627e83dfb45cfbc7aad10b9778f
Michel Edkrantz <michel.edkrantz@annotell.com>
2022-04-12 12:46:10 +0200

    Don't check refresh tll, reset on 401 instead. This will also work if refresh token has been invalidated

--- annotell-auth/annotell/auth/httpx/async_client.py
+++ annotell-auth/annotell/auth/httpx/async_client.py
@@ -1,21 +1,28 @@
 import logging
-from datetime import datetime, timedelta
+from asyncio import Lock
 from typing import Optional
 
+import httpx
 from authlib.integrations.httpx_client import AsyncOAuth2Client
 from authlib.oauth2.rfc6749 import OAuth2Token
 
-from annotell.auth import DEFAULT_HOST, REFRESH_TLL
+from annotell.auth import DEFAULT_HOST
 from annotell.auth.base.auth_client import AuthClient
-from annotell.auth.base.refresh_expiry_fix import RefreshTokenExpiryFixOAuth2Client
 from annotell.auth.credentials_parser import resolve_credentials
-from asyncio import Lock
 
 log = logging.getLogger(__name__)
 
 
-class _AsyncFixedClient(AsyncOAuth2Client, RefreshTokenExpiryFixOAuth2Client):
-    pass
+class _AsyncFixedClient(AsyncOAuth2Client):
+
+    async def _refresh_token(self, url, **kwargs):
+        try:
+            return await super(_AsyncFixedClient, self)._refresh_token(url, **kwargs)
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == 401:
+                log.info("Refresh token expired, resetting auth session")
+                return await self.fetch_token()
+            raise
 
 
 class HttpxAuthAsyncClient(AuthClient):
@@ -23,8 +30,7 @@ class HttpxAuthAsyncClient(AuthClient):
                  auth=None,
                  client_id: Optional[str] = None,
                  client_secret: Optional[str] = None,
-                 host: str = DEFAULT_HOST,
-                 refresh_ttl=REFRESH_TLL):
+                 host: str = DEFAULT_HOST):
         """
         There is a variety of ways to setup the authentication. See
         https://github.com/annotell/annotell-python/tree/master/annotell-auth
@@ -32,11 +38,9 @@ class HttpxAuthAsyncClient(AuthClient):
         :param client_id: client id for authentication
         :param client_secret: client secret for authentication
         :param host: base url for authentication server
-        :param refresh_ttl: Hint for refresh token expiry check
         """
         self.host = host
         self.token_url = "%s/v1/auth/oauth/token" % self.host
-        self._refresh_ttl = refresh_ttl
 
         client_id, client_secret = resolve_credentials(auth, client_id, client_secret)
 
@@ -57,7 +61,6 @@ class HttpxAuthAsyncClient(AuthClient):
         return self._oauth_client.token
 
     async def _update_token(self, token: OAuth2Token, refresh_token=None, access_token=None):
-        token["refresh_expires_at"] = datetime.utcnow() + timedelta(seconds=self._refresh_ttl)
         self._oauth_client.token = token
         self._log_new_token()
 
