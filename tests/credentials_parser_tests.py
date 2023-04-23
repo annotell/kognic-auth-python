87ae998bd2463fee041cabedd8771a59fb529043
Michel Edkrantz <michel.edkrantz@kognic.com>
2023-04-23 16:32:05 +0200

    fix gitignire and tests

--- tests/credentials_parser_tests.py
+++ tests/credentials_parser_tests.py
@@ -4,13 +4,13 @@ from kognic.auth import credentials_parser
 
 class CredentialsParserTest(unittest.TestCase):
   def test_parse_credentials(self):
-  	#p = {
-  	#	"clientId": "x"
-  	#	"clientSecret": "y"
-  	#	"email": "z"
-  	#	"userId": 1
-  	#	"issuer", "auth.kognic.test"
-  	#}
-  	#creds = credentials_parser.parse_credentials(p)
-  	self.assertEquals(1, 1)
-  	#self.assertEquals(creds.client_id, "x")
+  	p = {
+  		"clientId": "CLIENT_ID",
+  		"clientSecret": "SECRET",
+  		"email": "test@kognic.com",
+  		"userId": 1,
+  		"issuer": "auth.kognic.test",
+  	}
+  	creds = credentials_parser.parse_credentials(p)
+  	self.assertEquals(creds.client_id, "CLIENT_ID")
+    self.assertEquals(creds.client_secret, "SECRET")
