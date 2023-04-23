e2fc4cce826ccdc787f5f84621d837f4d12dc55c
Michel Edkrantz <michel.edkrantz@kognic.com>
2023-04-23 17:04:35 +0200

    fix

--- tests/credentials_parser_tests.py
+++ tests/credentials_parser_tests.py
@@ -3,14 +3,14 @@ from kognic.auth import credentials_parser
 
 
 class CredentialsParserTest(unittest.TestCase):
-  def test_parse_credentials(self):
-    p = {
-  		"clientId": "CLIENT_ID",
-  		"clientSecret": "SECRET",
-  		"email": "test@kognic.com",
-  		"userId": 1,
-  		"issuer": "auth.kognic.test",
-  	}
-  	creds = credentials_parser.parse_credentials(p)
-  	self.assertEquals(creds.client_id, "CLIENT_ID")
-    self.assertEquals(creds.client_secret, "SECRET")
+    def test_parse_credentials(self):
+        p = {
+            "clientId": "CLIENT_ID",
+            "clientSecret": "SECRET",
+            "email": "test@kognic.com",
+            "userId": 1,
+            "issuer": "auth.kognic.test",
+        }
+        creds = credentials_parser.parse_credentials(p)
+        self.assertEquals(creds.client_id, "CLIENT_ID")
+        self.assertEquals(creds.client_secret, "SECRET")
